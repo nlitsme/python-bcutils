166dfb5d1f1b4522d08454590fe37b7afc93f5ad
itsme <itsme@xs4all.nl>
2021-01-13 15:46:56 +0100

    making number conversion work with python3

--- convert.py
+++ convert.py
@@ -4,14 +4,8 @@ By Willem Hengeveld <itsme@xs4all.nl>
 convert between bytes and number
 """
 def numfrombytes(b):
-    return int(b.encode("hex"), 16)
+    return int.from_bytes(b, 'big')
 def bytesfromnum(num, n= 32):
-    b= "%x" % int(num)
-    if len(b)%2:
-        b= "0" + b
-    b= b.decode("hex")
-    if len(b)<n:
-        b= "\x00" * (n-len(b)) + b
-    return b
+    return int(num).to_bytes(32, 'big')
 
 
