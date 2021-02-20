47227c581910212cbb92e5b74f0cb2035ada9005
itsme <itsme@xs4all.nl>
2021-02-21 00:35:17 +0100

    hashing: now using the standard hashlib, instead of pycrypto or cryptography

--- hashing.py
+++ hashing.py
@@ -1,63 +1,37 @@
-try:
-    from Crypto.Hash import SHA256
-    try:
-        from Crypto.Hash import RIPEMD160
-    except:
-        from Crypto.Hash import RIPEMD as RIPEMD160
-except:
-    from cryptography.hazmat.primitives import hashes
-    from cryptography.hazmat.backends import default_backend
-    from cryptography import utils
-
-    @utils.register_interface(hashes.HashAlgorithm)
-    class MD160(object):
-        name = "ripemd160"
-        digest_size = 20
-        block_size = 64
-
-
-    class SHA256:
-        @staticmethod
-        def new():
-            return SHA256()
-        def __init__(self):
-            self.h = hashes.Hash(hashes.SHA256(), backend=default_backend())
-        def update(self, x):
-            self.h.update(x)
-        def digest(self):
-            return self.h.finalize()
-    class RIPEMD160:
-        @staticmethod
-        def new():
-            return RIPEMD160()
-        def __init__(self):
-            self.h = hashes.Hash(MD160(), backend=default_backend())
-        def update(self, x):
-            self.h.update(x)
-        def digest(self):
-            return self.h.finalize()
-
 """
 By Willem Hengeveld <itsme@xs4all.nl>
 
 two hashing operations used by bitcoin
 """
+import hashlib
 
+def sha256(*data):
+    """ Calculate a sha256 """
+    h1 = hashlib.new('sha256')
+    for x in data:
+        h1.update(x)
+    
+    return h1.digest()
 
-def shasha(*args):
-    h= SHA256.new()
-    for x in args:
-        h.update(x)
-    h2= SHA256.new()
-    h2.update(h.digest())
+def shasha(*data):
+    """ Calculate a transaction hash """
+    h1 = hashlib.new('sha256')
+    for x in data:
+        h1.update(x)
+    
+    h2 = hashlib.new('sha256')
+    h2.update( h1.digest() )
+
     return h2.digest()
 
+def sharip(data):
+    """ Calculate a address hash """
+    h1 = hashlib.new('sha256')
+    h1.update(data)
+    
+    h2 = hashlib.new('ripemd160')
+    h2.update( h1.digest() )
 
-def sharip(*args):
-    h= SHA256.new()
-    for x in args:
-        h.update(x)
-    h2= RIPEMD160.new()
-    h2.update(h.digest())
     return h2.digest()
+
 
