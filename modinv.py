76f23929469b532300ade80697b5f88b5c650cb6
itsme <itsme@xs4all.nl>
2022-11-11 22:13:00 +0100

    gcd now supports more than two arguments

--- modinv.py
+++ modinv.py
@@ -24,8 +24,16 @@ def modinv(x, m):
     return c
 
 
-def gcd(a, b):
-    (gcd, c, d) = GCD(a,b)
-    return gcd
+def gcd(*nums):
+    if len(nums)==1 and type(nums[0]) != int:
+        nums = nums[0]
+    g = None
+    for x in nums:
+        if g is None:
+            g = x
+        else:
+            (gcd, c, d) = GCD(g,x)
+            g = gcd
+    return g
 
 
