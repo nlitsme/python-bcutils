5279214d87ecf65ea941dddf2c95022f46cd405f
itsme <itsme@xs4all.nl>
2022-11-11 22:13:00 +0100

    added optional 'tag' argument to base58.encode

--- base58.py
+++ base58.py
@@ -1,6 +1,7 @@
 from __future__ import print_function, division
 from binascii import a2b_hex, b2a_hex
-
+import struct
+from hashing import shasha
 """
 By Willem Hengeveld <itsme@xs4all.nl>
 
@@ -9,9 +10,19 @@ base58 encode and decode
 
 charset= "123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz"
 
-def encode(data):
+def encode(*args):
+    """
+    encode a value as base58.
+    optionally takes a tag as it's first argument.
+    """
     def nbytes(x):
        return (x.bit_length()+7)//8
+
+    if len(args)==1:
+        data = args[0]
+    elif len(args)==2:
+        data = struct.pack("<B", args[0]) + args[1]
+        data += shasha(data)[0:4]
 
     res= ""
     if type(data)==bytes:
@@ -31,8 +42,8 @@ def encode(data):
         data //= 58
 
     res += charset[0] * nrzeros
-
     return res[::-1]
+
 
 def decode(enc):
     num= 0
