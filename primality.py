b367ff0a2e4138b43291d492bb421c776aae4b5a
itsme <itsme@xs4all.nl>
2022-11-11 22:13:00 +0100

    improved millerrabin test. added 'primesupto' function.

--- primality.py
+++ primality.py
@@ -1,21 +1,45 @@
 import random
+"""
+sieve and millerRabinTest
+
+https://oeis.org/A014233         Smallest odd number for which Miller-Rabin primality test on bases <= n-th prime does not reveal compositeness. 
+https://oeis.org/A006945         Smallest odd number that requires n Miller-Rabin primality tests. 
+https://oeis.org/A090659         Odd composites with increasing proportion of nontrivial non-witnesses of compositeness by the Miller-Rabin primality test. 
+  -> 9080191
+https://oeis.org/A340462         Triangular numbers that are the hypotenuse of a primitive Pythagorean triple (PPT) such that another member of the triple is also triangular. 
+  -> 4759123141
+https://oeis.org/A020287         Strong pseudoprimes to base 61. 
+  -> 4759123141
+
+https://oeis.org/A209834 		a(A074773(n) mod 1519829 mod 18) = A074773(n), 1 <= n <= 18. 
+
+https://miller-rabin.appspot.com/
+
+"""
 
 def millerRabinTest(n):
     def get_a_list(n):
         if n < 2047: alist = [2]
         elif n < 1373653: alist = [2, 3]
-        elif n < 9080191: alist = [31, 73]
+        #lif n < 9080191: alist = [31, 73]
         elif n < 25326001: alist = [2, 3, 5]
-        elif n < 3215031751: alist = [2, 3, 5, 7]
-        elif n < 4759123141: alist = [2, 7, 61]
-        elif n < 1122004669633: alist = [2, 13, 23, 1662803]
+        #lif n < 170584961: alist = [350, 3958281543]
+        elif n ==3215031751: alist = [0]          # only exception
+        #        4294967296    -- 2^32
+        #lif n < 4759123141: alist = [2, 7, 61]
+        #lif n < 75792980677: alist = [2, 379215, 457083754]
+        elif n < 118670087467: alist = [2, 3, 5, 7]   #  -> n= 3215031751  or prime
+        #lif n < 1122004669633: alist = [2, 13, 23, 1662803]
         elif n < 2152302898747: alist = [2, 3, 5, 7, 11]
         elif n < 3474749660383: alist = [2, 3, 5, 7, 11, 13]
+        #lif n < 21652684502221: alist = [2, 1215, 34862, 574237825]
         elif n < 341550071728321: alist = [2, 3, 5, 7, 11, 13, 17]
         elif n < 3825123056546413051: alist = [2, 3, 5, 7, 11, 13, 17, 19, 23]
-        elif n < 18446744073709551616: alist = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37]
+        #        18446744073709551616   -- 2^64
         elif n < 318665857834031151167461: alist = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37]
         elif n < 3317044064679887385961981: alist = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41]
+        #        79228162514264337593543950336   -- 2^96
+        #        340282366920938463463374607431768211456  -- 2^128
         else: alist = None
 
         if alist:
@@ -49,6 +73,8 @@ def millerRabinTest(n):
         return False
     chance = 1
     for a in get_a_list(n):
+        if not a:
+            return False
         if not oneTest(a):
             return False
         chance *= 4
@@ -56,7 +82,9 @@ def millerRabinTest(n):
             break
     return True
 
+
 def nextPrime(num):
+    if num<2: return 2
     num |= 1
     while not millerRabinTest(num):
         num += 2
@@ -86,6 +114,7 @@ def generatePrimes(start=2, end=None):
         yield num
         num += 2
 
+
 def sieve(num):
     """
     import primality
@@ -101,4 +130,14 @@ def sieve(num):
             for i in range(p*p, num, p):
                 map[i]= True
         p += 1
+
+def primesupto(n):
+    # https://stackoverflow.com/questions/2068372/fastest-way-to-list-all-primes-below-n-in-python/3035188#3035188
+    """ Returns  a list of primes < n """
+    sieve = [True] * (n//2)
+    for i in range(3,int(n**0.5)+1,2):
+        if sieve[i//2]:
+            sieve[i*i//2::i] = [False] * ((n-i*i-1)//(2*i)+1)
+    return [2] + [2*i+1 for i in range(1,n//2) if sieve[i]]
+
 
