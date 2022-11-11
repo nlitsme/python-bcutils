ed6d39a6e9edee01ab44c3ea5455b24bb84eadf1
itsme <itsme@xs4all.nl>
2022-11-11 22:13:00 +0100

    md160 was dropped from hashlib, so now I have to use pycrypto / pycryptodome

--- hashing.py
+++ hashing.py
@@ -3,11 +3,12 @@ By Willem Hengeveld <itsme@xs4all.nl>
 
 two hashing operations used by bitcoin
 """
-import hashlib
+import Crypto.Hash.SHA256
+import Crypto.Hash.RIPEMD160
 
 def sha256(*data):
     """ Calculate a sha256 """
-    h1 = hashlib.new('sha256')
+    h1 = Crypto.Hash.SHA256.new()
     for x in data:
         h1.update(x)
     
@@ -15,21 +16,21 @@ def sha256(*data):
 
 def shasha(*data):
     """ Calculate a transaction hash """
-    h1 = hashlib.new('sha256')
+    h1 = Crypto.Hash.SHA256.new()
     for x in data:
         h1.update(x)
     
-    h2 = hashlib.new('sha256')
+    h2 = Crypto.Hash.SHA256.new()
     h2.update( h1.digest() )
 
     return h2.digest()
 
 def sharip(data):
     """ Calculate a address hash """
-    h1 = hashlib.new('sha256')
+    h1 = Crypto.Hash.SHA256.new()
     h1.update(data)
     
-    h2 = hashlib.new('ripemd160')
+    h2 = Crypto.Hash.RIPEMD160.new()
     h2.update( h1.digest() )
 
     return h2.digest()
