b95a737c5e20a53786d07f825b279bc5bfcf8236
itsme <itsme@xs4all.nl>
2025-06-30 12:33:39 +0200

    support both cryptodome and pycrypto

--- hashing.py
+++ hashing.py
@@ -3,12 +3,14 @@ By Willem Hengeveld <itsme@xs4all.nl>
 
 two hashing operations used by bitcoin
 """
-import Crypto.Hash.SHA256
-import Crypto.Hash.RIPEMD160
+try:
+    from Crypto.Hash import  SHA256, RIPEMD160
+except ImportError:
+    from Cryptodome.Hash import  SHA256, RIPEMD160
 
 def sha256(*data):
     """ Calculate a sha256 """
-    h1 = Crypto.Hash.SHA256.new()
+    h1 = SHA256.new()
     for x in data:
         h1.update(x)
     
@@ -16,21 +18,21 @@ def sha256(*data):
 
 def shasha(*data):
     """ Calculate a transaction hash """
-    h1 = Crypto.Hash.SHA256.new()
+    h1 = SHA256.new()
     for x in data:
         h1.update(x)
     
-    h2 = Crypto.Hash.SHA256.new()
+    h2 = SHA256.new()
     h2.update( h1.digest() )
 
     return h2.digest()
 
 def sharip(data):
     """ Calculate a address hash """
-    h1 = Crypto.Hash.SHA256.new()
+    h1 = SHA256.new()
     h1.update(data)
     
-    h2 = Crypto.Hash.RIPEMD160.new()
+    h2 = RIPEMD160.new()
     h2.update( h1.digest() )
 
     return h2.digest()
